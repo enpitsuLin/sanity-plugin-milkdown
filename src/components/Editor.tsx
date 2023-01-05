518e901fc797609fddc84d8a356369f87da252fb
enpitsulin <enpitsulin@gmail.com>
2023-01-05 11:02:23 +0800

    feat: add material-icon font

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,6 +1,7 @@
 import {forwardRef, lazy, useState} from 'react'
 import {StringInputProps} from 'sanity'
 import {Card} from '@sanity/ui'
+import {HelmetData, Helmet} from 'react-helmet-async'
 import styled from 'styled-components'
 
 const StyledCard = styled(Card)`
@@ -10,12 +11,26 @@ const StyledCard = styled(Card)`
 
 const MilkDownEditor = lazy(() => import('./MilkdownEditor'))
 
+const helmetData = new HelmetData({})
+
 export const MarkdownEditorMilkdown = forwardRef<HTMLDivElement, StringInputProps>((props, ref) => {
   const {value = '', readOnly} = props
 
   const [editedValue, setEditedValue] = useState<string>(value)
   return (
     <div ref={ref}>
+      <Helmet helmetData={helmetData}>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
+        />
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined"
+        />
+      </Helmet>
       <StyledCard>
         <MilkDownEditor value={editedValue} onChange={setEditedValue} readOnly={readOnly} />
       </StyledCard>
